a7a4edd93586ba38036e8dc6dace90f98966dad0
jush dayan <jushdayan@jushs-MacBook-Air-2.local>
2026-03-27 10:09:23 +0300

    Force CSS update v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'sudoku-v1';
-const ASSETS = ['./', './index.html', './css/styles.css', './js/sudoku.js', './js/ui.js', './manifest.json'];
+const ASSETS = ['./', './index.html', './css/styles_v3.css', './js/sudoku.js', './js/ui.js', './manifest.json'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
